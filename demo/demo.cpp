20363f1378c7d913ef7114d19d667aa2f57e8451
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2017-10-22 11:59:01 +0200

    demo code

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -1,6 +1,76 @@
 
+#include <gbBase/Finally.hpp>
+#include <gbBase/Log.hpp>
+#include <gbBase/LogHandlers.hpp>
+
 #include <gbVk/config.hpp>
+#include <gbVk/Instance.hpp>
+#include <gbVk/PhysicalDevice.hpp>
+#include <gbVk/StringConverters.hpp>
+
+
+#include <GLFW/glfw3.h>
+
+#include <cstring>
+#include <memory>
+#include <vector>
+
+
+struct DemoState {};
 
 int main()
 {
+    Ghulbus::Log::initializeLogging();
+    auto const gblog_init_guard = Ghulbus::finally([]() { Ghulbus::Log::shutdownLogging(); });
+    Ghulbus::Log::Handlers::LogSynchronizeMutex logger(Ghulbus::Log::Handlers::logToCout);
+    Ghulbus::Log::setLogHandler(logger);
+    Ghulbus::Log::setLogLevel(Ghulbus::LogLevel::Trace);
+
+    if(!glfwInit())
+    {
+        return 1;
+    }
+    auto const glfw_init_guard = Ghulbus::finally([]() { glfwTerminate(); });
+    glfwSetErrorCallback([](int ec, char const* msg) { GHULBUS_LOG(Error, "GLFW Error " << ec << " - " << msg); });
+    if(!glfwVulkanSupported()) {
+        GHULBUS_LOG(Error, "No Vulkan support in GLFW.");
+        return 1;
+    }
+    GHULBUS_LOG(Trace, "GLFW " << glfwGetVersionString() << " up and running.");
+
+    GhulbusVulkan::Instance instance = GhulbusVulkan::Instance::createInstance();
+
+    auto phys_devices = instance.enumeratePhysicalDevices();
+    auto dev_props = phys_devices.front().getProperties();
+    GHULBUS_LOG(Info, "Using device " << dev_props.deviceName << " ("
+                      << "Vulkan Version " << GhulbusVulkan::version_to_string(dev_props.apiVersion) << ", "
+                      << "Driver Version " << GhulbusVulkan::version_to_string(dev_props.driverVersion)
+                      << ").");
+
+    VkDevice device = phys_devices.front().createDevice();
+    auto finalizer_device = Ghulbus::finally([&device]() { vkDestroyDevice(device, nullptr); });
+
+    glfwWindowHint(GLFW_RESIZABLE, 0);
+    auto main_window =
+        std::unique_ptr<GLFWwindow, void(*)(GLFWwindow*)>(glfwCreateWindow(1280, 720, "Vulkan Demo", nullptr, nullptr),
+                                                          glfwDestroyWindow);
+    if(!main_window) {
+        return 1;
+    }
+
+    DemoState state;
+    glfwSetWindowUserPointer(main_window.get(), &state);
+
+    glfwSetKeyCallback(main_window.get(),
+        [](GLFWwindow* window, int key, int scancode, int action, int mods)
+        {
+            if(key == GLFW_KEY_ESCAPE) {
+                glfwSetWindowShouldClose(window, true);
+            }
+        });
+
+    GHULBUS_LOG(Trace, "Entering main loop...");
+    while(!glfwWindowShouldClose(main_window.get())) {
+        glfwPollEvents();
+    }
 }
