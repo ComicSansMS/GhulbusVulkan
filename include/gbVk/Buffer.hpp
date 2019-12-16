079fcd224ee2b381cd47c43181807afd69a8f13f
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2019-12-16 14:31:04 +0100

    buffer now knows transitions

--- include/gbVk/Buffer.hpp
+++ include/gbVk/Buffer.hpp
@@ -16,6 +16,8 @@
 
 namespace GHULBUS_VULKAN_NAMESPACE
 {
+class CommandBuffer;
+
 class Buffer {
 private:
     VkBuffer m_buffer;
@@ -29,6 +31,22 @@ public:
 
     Buffer(Buffer&& rhs);
     Buffer& operator=(Buffer&& rhs) = delete;
+
+    void transitionRelease(CommandBuffer& command_buffer, VkPipelineStageFlags src_stage,
+                           VkPipelineStageFlags dst_stage, VkAccessFlags src_access_mask,
+                           uint32_t dst_queue_family);
+
+    void transitionRelease(CommandBuffer& command_buffer, VkPipelineStageFlags src_stage,
+                           VkPipelineStageFlags dst_stage, VkAccessFlags src_access_mask,
+                           uint32_t dst_queue_family, VkDeviceSize offset, VkDeviceSize size);
+
+    void transitionAcquire(CommandBuffer& command_buffer, VkPipelineStageFlags src_stage,
+                           VkPipelineStageFlags dst_stage, VkAccessFlags dst_access_mask,
+                           uint32_t src_queue_family);
+
+    void transitionAcquire(CommandBuffer& command_buffer, VkPipelineStageFlags src_stage,
+                           VkPipelineStageFlags dst_stage, VkAccessFlags dst_access_mask,
+                           uint32_t src_queue_family, VkDeviceSize offset, VkDeviceSize size);
 
     VkBuffer getVkBuffer();
 
--- src/Buffer.cpp
+++ src/Buffer.cpp
@@ -1,7 +1,10 @@
 #include <gbVk/Buffer.hpp>
 
+#include <gbVk/CommandBuffer.hpp>
 #include <gbVk/DeviceMemory.hpp>
 #include <gbVk/Exceptions.hpp>
+
+#include <gbBase/Assert.hpp>
 
 namespace GHULBUS_VULKAN_NAMESPACE
 {
@@ -43,5 +46,55 @@ void Buffer::bindBufferMemory(DeviceMemory& memory, VkDeviceSize offset)
 {
     VkResult const res = vkBindBufferMemory(m_device, m_buffer, memory.getVkDeviceMemory(), offset);
     checkVulkanError(res, "Error in vkBindBufferMemory.");
+}
+
+void Buffer::transitionRelease(CommandBuffer& command_buffer, VkPipelineStageFlags src_stage,
+                               VkPipelineStageFlags dst_stage, VkAccessFlags src_access_mask,
+                               uint32_t dst_queue_family)
+{
+    transitionRelease(command_buffer, src_stage, dst_stage, src_access_mask, dst_queue_family, 0, VK_WHOLE_SIZE);
+}
+
+void Buffer::transitionRelease(CommandBuffer& command_buffer, VkPipelineStageFlags src_stage,
+                               VkPipelineStageFlags dst_stage, VkAccessFlags src_access_mask,
+                               uint32_t dst_queue_family, VkDeviceSize offset, VkDeviceSize size)
+{
+    VkBufferMemoryBarrier buffer_barr;
+    buffer_barr.sType = VK_STRUCTURE_TYPE_BUFFER_MEMORY_BARRIER;
+    buffer_barr.pNext = nullptr;
+    buffer_barr.srcAccessMask = src_access_mask;
+    buffer_barr.dstAccessMask = 0;
+    buffer_barr.srcQueueFamilyIndex = command_buffer.getQueueFamilyIndex();
+    buffer_barr.dstQueueFamilyIndex = dst_queue_family;
+    buffer_barr.buffer = m_buffer;
+    buffer_barr.offset = offset;
+    buffer_barr.size = size;
+    vkCmdPipelineBarrier(command_buffer.getVkCommandBuffer(), src_stage, dst_stage, 0,
+                         0, nullptr, 1, &buffer_barr, 0, nullptr);
+}
+
+void Buffer::transitionAcquire(CommandBuffer& command_buffer, VkPipelineStageFlags src_stage,
+                               VkPipelineStageFlags dst_stage, VkAccessFlags dst_access_mask,
+                               uint32_t src_queue_family)
+{
+    transitionAcquire(command_buffer, src_stage, dst_stage, dst_access_mask, src_queue_family, 0, VK_WHOLE_SIZE);
+}
+
+void Buffer::transitionAcquire(CommandBuffer& command_buffer, VkPipelineStageFlags src_stage,
+                               VkPipelineStageFlags dst_stage, VkAccessFlags dst_access_mask,
+                               uint32_t src_queue_family, VkDeviceSize offset, VkDeviceSize size)
+{
+    VkBufferMemoryBarrier buffer_barr;
+    buffer_barr.sType = VK_STRUCTURE_TYPE_BUFFER_MEMORY_BARRIER;
+    buffer_barr.pNext = nullptr;
+    buffer_barr.srcAccessMask = 0;
+    buffer_barr.dstAccessMask = dst_access_mask;
+    buffer_barr.srcQueueFamilyIndex = src_queue_family;
+    buffer_barr.dstQueueFamilyIndex = command_buffer.getQueueFamilyIndex();
+    buffer_barr.buffer = m_buffer;
+    buffer_barr.offset = offset;
+    buffer_barr.size = size;
+    vkCmdPipelineBarrier(command_buffer.getVkCommandBuffer(), src_stage, dst_stage, 0,
+                         0, nullptr, 1, &buffer_barr, 0, nullptr);
 }
 }
