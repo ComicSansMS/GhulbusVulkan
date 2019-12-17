21c6fcb1890f7fae52e063403ff832779f36d87b
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2019-12-17 10:42:51 +0100

    added VMA debug flags (commented out)

--- src/gbGraphics/detail/VulkanMemoryAllocator.cpp
+++ src/gbGraphics/detail/VulkanMemoryAllocator.cpp
@@ -6,6 +6,9 @@
 #endif
 
 #define VMA_IMPLEMENTATION
+//#define VMA_DEBUG_INITIALIZE_ALLOCATIONS 1
+//#define VMA_DEBUG_MARGIN 16
+//#define VMA_DEBUG_DETECT_CORRUPTION 1
 #include <vk_mem_alloc.h>
 
 #if _MSC_VER
