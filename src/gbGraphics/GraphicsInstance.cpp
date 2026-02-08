3bab8d453f43b8644f5512c06f8829523f1f5530
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2026-02-08 08:21:42 +0100

    clean up debug log handler setting

--- src/gbGraphics/GraphicsInstance.cpp
+++ src/gbGraphics/GraphicsInstance.cpp
@@ -318,12 +318,7 @@ void GraphicsInstance::setDebugLoggingEnabled(bool enabled)
 {
     if (enabled) {
         if (!m_pimpl->debug_logging) {
-            VkDebugReportFlagsEXT const all_the_flags = VK_DEBUG_REPORT_INFORMATION_BIT_EXT |
-                                                        VK_DEBUG_REPORT_WARNING_BIT_EXT |
-                                                        VK_DEBUG_REPORT_PERFORMANCE_WARNING_BIT_EXT |
-                                                        VK_DEBUG_REPORT_ERROR_BIT_EXT |
-                                                        VK_DEBUG_REPORT_DEBUG_BIT_EXT;
-            m_pimpl->debug_logging.emplace(m_pimpl->instance, all_the_flags);
+            m_pimpl->debug_logging.emplace(m_pimpl->instance);
             m_pimpl->debug_logging->addCallback(
                 [](VkDebugReportFlagsEXT flags, VkDebugReportObjectTypeEXT object_type,
                    uint64_t object, size_t location, int32_t message_code,
