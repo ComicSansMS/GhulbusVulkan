9afac135e408362869edde1eae415a5eb2550e0c
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2026-02-09 18:25:06 +0100

    add multi wait functions for fence

--- include/gbVk/Fence.hpp
+++ include/gbVk/Fence.hpp
@@ -13,6 +13,9 @@
 #include <vulkan/vulkan.hpp>
 
 #include <chrono>
+#include <concepts>
+#include <cstddef>
+#include <span>
 
 namespace GHULBUS_VULKAN_NAMESPACE
 {
@@ -47,6 +50,36 @@ public:
     Status wait_for(std::chrono::nanoseconds timeout);
 
     void reset();
+
+    template<typename... Args> requires(std::same_as<Args, Fence> &&...)
+    static Status wait_all(Args&... fences) {
+        return wait_all_for(std::chrono::nanoseconds::max(), fences...);
+    }
+
+    template<typename... Args> requires(std::same_as<Args, Fence> &&...)
+    static Status wait_all_for(std::chrono::nanoseconds timeout, Args&... fences) {
+        static_assert(sizeof...(Args) < 1024);
+        VkFence vk_fences[sizeof...(Args)] = { fences.getVkFence()... };
+        VkDevice vk_devices[sizeof...(Args)] = { fences.m_device... };
+        return (wait_impl(vk_devices, vk_fences, timeout, true) == 0) ? Status::Ready : Status::NotReady;
+    }
+
+    template<typename... Args> requires(std::same_as<Args, Fence> &&...)
+    static std::size_t wait_any(Args&... fences) {
+        return wait_any_for(std::chrono::nanoseconds::max(), fences...);
+    }
+
+    template<typename... Args> requires(std::same_as<Args, Fence> &&...)
+    static std::size_t wait_any_for(std::chrono::nanoseconds timeout, Args&... fences) {
+        static_assert(sizeof...(Args) < 1024);
+        VkFence vk_fences[sizeof...(Args)] = { fences.getVkFence()... };
+        VkDevice vk_devices[sizeof...(Args)] = { fences.m_device... };
+        return wait_impl(vk_devices, vk_fences, timeout, false);
+    }
+
+private:
+    static size_t wait_impl(std::span<VkDevice> vk_devices, std::span<VkFence> vk_fences,
+                            std::chrono::nanoseconds timeout, bool wait_all);
 };
 }
 #endif
--- src/Fence.cpp
+++ src/Fence.cpp
@@ -5,6 +5,9 @@
 
 #include <gbBase/Assert.hpp>
 
+#include <algorithm>
+#include <limits>
+
 namespace GHULBUS_VULKAN_NAMESPACE
 {
 Fence::Fence(VkDevice logical_device, VkFence fence)
@@ -36,7 +39,7 @@ VkFence Fence::getVkFence()
 
 Fence::Status Fence::getStatus()
 {
-    VkResult res = vkGetFenceStatus(m_device, m_fence);
+    VkResult const res = vkGetFenceStatus(m_device, m_fence);
     if(res == VK_NOT_READY) { return Status::NotReady; }
     checkVulkanError(res, "Error in vkGetFenceStatus.");
     return Status::Ready;
@@ -50,8 +53,8 @@ void Fence::wait()
 
 Fence::Status Fence::wait_for(std::chrono::nanoseconds timeout)
 {
-    VkResult res = vkWaitForFences(m_device, 1, &m_fence, VK_TRUE, timeout.count());
-    if(res == VK_NOT_READY) { return Status::NotReady; }
+    VkResult const res = vkWaitForFences(m_device, 1, &m_fence, VK_TRUE, timeout.count());
+    if(res == VK_TIMEOUT) { return Status::NotReady; }
     checkVulkanError(res, "Error in vkWaitForFences.");
     return Status::Ready;
 }
@@ -61,4 +64,37 @@ void Fence::reset()
     VkResult res = vkResetFences(m_device, 1, &m_fence);
     checkVulkanError(res, "Error in vkResetFences.");
 }
+
+/* static */ std::size_t Fence::wait_impl(std::span<VkDevice> vk_devices,
+                                          std::span<VkFence> vk_fences,
+                                          std::chrono::nanoseconds timeout,
+                                          bool wait_all)
+{
+    GHULBUS_ASSERT(vk_devices.size() == vk_fences.size());
+    if (!(std::all_of(begin(vk_devices), end(vk_devices),
+                      [&](VkDevice d) { return d == vk_devices.front(); })))
+    {
+        GHULBUS_THROW(Exceptions::VulkanError(), "Attempt to wait on fences from different devices");
+    }
+    VkResult const res = vkWaitForFences(
+        vk_devices.front(),
+        static_cast<uint32_t>(vk_fences.size()),
+        vk_fences.data(),
+        (wait_all) ? VK_TRUE : VK_FALSE,
+        timeout.count()
+    );
+    if (res == VK_TIMEOUT) { return std::numeric_limits<std::size_t>::max(); }
+    checkVulkanError(res, "Error in vkWaitForFences.");
+    if (!wait_all) {
+        // check which fence is ready
+        for (std::size_t i = 0; i < vk_devices.size(); ++i) {
+            VkResult const res_i = vkGetFenceStatus(vk_devices[i], vk_fences[i]);
+            if (res_i == VK_SUCCESS) { return i; }
+            checkVulkanError(res_i, "Error in vkGetFenceStatus.");
+        }
+        GHULBUS_UNREACHABLE_MESSAGE("No fence is ready even though vkWaitForFences returned");
+    }
+    return 0;
+}
+
 }
